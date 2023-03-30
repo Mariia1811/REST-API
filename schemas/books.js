ea3df568c01b153b4bd138fbc51112ab9cc54e12
Mariia Kryzhalko <kruzhalko93@gmail.com>
2023-03-30 12:55:55 +0300

    refactoring routes

---
+++ schemas/books.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+const addContactsSchema = Joi.object({
+    name: Joi.string().required().messages({
+      "any.required": `missing required "name" field`,
+      "string.base": `"name" must be string`,
+      "string.empty": `"name" cannot be empty`,
+    }),
+    email: Joi.string().required().messages({
+      "any.required": `missing required "email" field`,
+      "string.base": `"email" must be string`,
+      "string.empty": `"email" cannot be empty`,
+    }),
+    phone: Joi.string().required().messages({
+      "any.required": `missing required "phone" field`,
+      "string.base": `"phone" must be string`,
+      "string.empty": `"phone" cannot be empty`,
+    }),
+  });
+
+  module.exports = addContactsSchema;
